657817f92ac8c83e9873628c5977291e567e83ea
DavCohK <david.cohen@khealth.com>
2024-10-22 19:23:55 +0300

    try to fix static files

--- bigday/language_router.py
+++ bigday/language_router.py
@@ -3,13 +3,36 @@ from django.http import HttpResponseRedirect
 from django.urls import translate_url
 from django.utils import translation
 
+import os
+
+def print_directory_contents(path):
+    for root, dirs, files in os.walk(path):
+        # Print the current directory path
+        print(f"Directory: {root}")
+        
+        # Print all subdirectories in the current directory
+        for dir_name in dirs:
+            print(f"  Subdirectory: {dir_name}")
+        
+        # Print all files in the current directory
+        for file_name in files:
+            print(f"  File: {file_name}")
+
+# Get the current working directory
+
+# Print the contents of the current directory
+
 class LanguageRouterMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         # Exclude static and media files from redirection
-        if request.path.startswith(settings.STATIC_URL) or request.path.startswith(settings.MEDIA_URL):
+        current_directory = os.getcwd()
+        print_directory_contents(current_directory)
+        print("request.path", request.path)
+        print(os.listdir())
+        if request.path.startswith(settings.STATIC_URL):
             return self.get_response(request)
 
         if request.path.startswith('/en') or request.path.startswith('/fr'):
@@ -18,6 +41,7 @@ class LanguageRouterMiddleware:
 
         # Get language from the session or browser
         language = request.session.get('django_language', request.LANGUAGE_CODE)
+        print("language", language)
         if not language:
             language = translation.get_language_from_request(request)
 
